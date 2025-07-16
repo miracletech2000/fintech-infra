4e1f713c67fad83860117f464ed49c916cb023b5
Miracle Oluwole <miracle@Miracles-Laptop.lan>
2025-07-16 09:50:42 -0600

    ../modules/eks-cluster/main.tf

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -110,7 +110,7 @@ module "eks" {
       principal_arn     = "arn:aws:iam::999568710647:user/nfusi"
       policy_associations = [
         {
-          policy_arn  = "arn:aws:eks:831926583884:aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          policy_arn  = "arn:aws:eks:999568710647:aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = { type = "cluster" }
         }
       ]
